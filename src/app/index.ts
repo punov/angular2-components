efd7f67903b31ac6e4b79b54dcd176b6369970ef
punov <design.qs@gmail.com>
2015-09-25 04:36:12 +0300

    Renamed app

---
+++ src/app/index.ts
@@ -0,0 +1,5 @@
+import { bootstrap } from 'angular2/angular2';
+import { AngularApp } from './app-module/app-module';
+import {CmsHomeComponent} from './home-module/home-module';
+
+bootstrap(AngularApp, [CmsHomeComponent]);
